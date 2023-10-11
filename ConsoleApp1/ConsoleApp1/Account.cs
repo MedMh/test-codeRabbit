a9180fa79efa67c6da7ffd103631f18958633ed7
Mohamed Abdel Khalek Mhedhbi <ma.khalekmhedhbi@instadeep.com>
2023-10-11 23:01:41 +0100

    put saving account into different file

--- ConsoleApp1/ConsoleApp1/Account.cs
+++ ConsoleApp1/ConsoleApp1/Account.cs
@@ -35,19 +35,3 @@ class Account
         return $"Account Number: {AccountNumber}, Holder: {AccountHolder}, Balance: {Balance:C}";
     }
 }
-
-class SavingsAccount : Account
-{
-    private decimal InterestRate;
-
-    public SavingsAccount(int accountNumber, string accountHolder, decimal initialBalance, decimal interestRate)
-        : base(accountNumber, accountHolder, initialBalance)
-    {
-        InterestRate = interestRate;
-    }
-
-    public void ApplyInterest()
-    {
-        Balance += Balance * InterestRate;
-    }
-}
---
+++ ConsoleApp1/ConsoleApp1/SavingAccount.cs
@@ -0,0 +1,15 @@
+﻿class SavingsAccount : Account
+{
+    private decimal InterestRate;
+
+    public SavingsAccount(int accountNumber, string accountHolder, decimal initialBalance, decimal interestRate)
+        : base(accountNumber, accountHolder, initialBalance)
+    {
+        InterestRate = interestRate;
+    }
+
+    public void ApplyInterest()
+    {
+        Balance += Balance * InterestRate;
+    }
+}
